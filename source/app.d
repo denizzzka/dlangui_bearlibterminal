5bb9bdaa6175aeeb5f2743480a78c91a9dfa3df0
Denis Feklushkin <denis.feklushkin@gmail.com>
2017-07-16 13:55:16 +0700

    BearLibDrawBuf : DrawBuf

--- source/app.d
+++ source/app.d
@@ -220,7 +220,7 @@ class BearLibWindow : Window
     }
 }
 
-class BearLibDrawBuf : ColorDrawBuf
+class BearLibDrawBuf : DrawBuf
 {
     // Since only one window is allowed with BearLib terminal
     private BearLibWindow window;
@@ -228,8 +228,54 @@ class BearLibDrawBuf : ColorDrawBuf
     this(BearLibWindow w)
     {
         window = w;
+    }
 
-        super(w.width, w.height);
+    override:
+
+    @property int width() { return window.width; }
+    @property int height() { return window.height; }
+
+    void resize(int _width, int _height)
+    {
+        if(width == _width && width == _height)
+            return;
+        else
+            assert(false, __FUNCTION__~": resizing isn't implemented");
+
+        //~ _dx = width;
+        //~ _dy = height;
+
+        //~ resetClipping();
+    }
+
+    void fill(uint color)
+    {
+        assert(false, __FUNCTION__~" isn't implemented");
+    }
+
+    void fillRect(Rect rc, uint color)
+    {
+        assert(false, __FUNCTION__~" isn't implemented");
+    }
+
+    void drawPixel(int x, int y, uint color)
+    {
+        assert(false, __FUNCTION__~" isn't implemented");
+    }
+
+    void drawGlyph(int x, int y, Glyph* glyph, uint color)
+    {
+        assert(false, __FUNCTION__~" isn't implemented");
+    }
+
+    void drawFragment(int x, int y, DrawBuf src, Rect srcrect)
+    {
+        assert(false, __FUNCTION__~" isn't implemented");
+    }
+
+    void drawRescaled(Rect dstrect, DrawBuf src, Rect srcrect)
+    {
+        assert(false, __FUNCTION__~" isn't implemented");
     }
 }
 
