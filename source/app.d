73e375c6bfae5b35b974dad4f7981018d298cdde
Denis Feklushkin <denis.feklushkin@gmail.com>
2017-07-19 17:52:52 +0700

    button click delegate added

--- source/app.d
+++ source/app.d
@@ -395,7 +395,17 @@ extern (C) int UIAppMain(string[] args)
     window.mainWidget.addChild = new TextWidget(null, "НЕЧТО ДЛИННОЕ ПРЕДЛИННОЕ"d);
     window.mainWidget.addChild = new TextWidget(null, "Покороче"d);
     window.mainWidget.addChild = (new Button).text("Button 1"d);
-    window.mainWidget.addChild = (new Button).text("Button 2"d);
+
+    auto btn2 = (new Button).text("Button 2"d);
+
+    btn2.click =
+        delegate(Widget w)
+        {
+            Log.d("Button click!");
+            return true;
+        };
+
+    window.mainWidget.addChild = btn2;
 
     //~ window.mainWidget.measure(SIZE_UNSPECIFIED, SIZE_UNSPECIFIED);
 
