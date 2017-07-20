6c6c90728b03e71beab494d93acc1985db783c87
Denis Feklushkin <denis.feklushkin@gmail.com>
2017-07-20 21:38:20 +0700

    Platform and Window moved to separate files

---
+++ source/platform.d
@@ -0,0 +1,103 @@
+module dlangui_bearlibterminal.platform;
+
+import dlangui;
+import dlangui.platforms.common.platform;
+
+class BearLibPlatform : Platform
+{
+    import dlangui_bearlibterminal.window: BearLibWindow;
+    import BearLibTerminal: BT = terminal;
+
+    private BearLibWindow window;
+
+    override:
+
+    BearLibWindow createWindow(dstring windowCaption, Window parent, uint flags, uint width, uint height)
+    {
+        assert(window is null);
+
+        window = new BearLibWindow(windowCaption);
+
+        return window;
+    }
+
+    void closeWindow(Window w)
+    {
+        assert(w == window);
+        assert(window !is null);
+
+        w.close();
+        destroy(w);
+
+        window = null;
+    }
+
+    /**
+    * Starts application message loop.
+    *
+    * When returned from this method, application is shutting down.
+    */
+    int enterMessageLoop()
+    {
+        do
+        {
+            if(BT.has_input)
+            {
+                auto event = BT.read();
+
+                Log.d("MessageLoop event = "~event.to!string);
+
+                with(BT)
+                switch(event)
+                {
+                    case keycode.close:
+                        destroy(window);
+                        Log.d("return 0");
+                        return 0;
+
+                    case keycode.resized:
+                        window.updateDlanguiWindowSize();
+                        window.show();
+                        break;
+
+                    default:
+                        import core.bitop: btr;
+                        import dlangui_bearlibterminal.events;
+
+                        const bool keyReleased = btr(cast(size_t*) &event, 8) != 0;
+
+                        {
+                            auto ke = convertKeyEvent(event, keyReleased);
+                            if(ke !is null)
+                                window.dispatchKeyEvent(ke);
+                        }
+
+                        {
+                            auto me = convertMouseEvent(event, keyReleased);
+                            if(me !is null)
+                                window.dispatchMouseEvent(me);
+                        }
+
+                        break;
+                }
+            }
+        }
+        while(true);
+    }
+
+    dstring getClipboardText(bool mouseBuffer)
+    {
+        assert(false, "Isn't implemented");
+    }
+
+    void setClipboardText(dstring text, bool mouseBuffer)
+    {
+        assert(false, "Isn't implemented");
+    }
+
+    void requestLayout()
+    {
+        if(window !is null)
+            window.requestLayout();
+    }
+}
---
+++ source/window.d
@@ -0,0 +1,110 @@
+module dlangui_bearlibterminal.window;
+
+import dlangui;
+
+class BearLibWindow : Window
+{
+    import BearLibTerminal: BT = terminal;
+
+    this(dstring caption)
+    {
+        windowOrContentResizeMode = WindowOrContentResizeMode.shrinkWidgets;
+        super();
+
+        BT.open(caption.to!string);
+        BT.set("window.resizeable=true");
+        BT.set("input.filter={keyboard+, mouse+}");
+
+        // set background color:
+        {
+            BT.bkcolor(backgroundColor);
+            BT.clear();
+            BT.refresh();
+        }
+
+        updateDlanguiWindowSize();
+
+        //FIXME: why this is need here?
+        updateWindowOrContentSize();
+    }
+
+    ~this()
+    {
+        close();
+    }
+
+    package void updateDlanguiWindowSize()
+    {
+        with(BT)
+        {
+            onResize(
+                state(keycode.width),
+                state(keycode.height)
+            );
+        }
+    }
+
+    private void redraw()
+    {
+        import dlangui_bearlibterminal.drawbuf;
+
+        BT.clear();
+
+        BearLibDrawBuf buf = new BearLibDrawBuf(width, height);
+
+        onDraw(buf);
+
+        BT.refresh();
+
+        destroy(buf);
+    }
+
+    override:
+
+    void close()
+    {
+        BT.close();
+    }
+
+    void show()
+    {
+        static bool firstCall = true;
+
+        if(firstCall)
+        {
+            firstCall = false;
+
+            invalidate();
+        }
+
+        {
+            import dlangui.widgets.widget;
+
+            if(mainWidget !is null)
+            {
+                Log.d("_mainWidget available");
+
+                redraw();
+            }
+        }
+    }
+
+    dstring windowCaption() @property
+    {
+        return BT.get("window.title", "default_value").to!dstring;
+    }
+
+    void windowCaption(dstring caption) @property
+    {
+        BT.setf("window.title=%s", caption);
+    }
+
+    void windowIcon(DrawBufRef icon) @property
+    {
+        assert(false, "Isn't implemented");
+    }
+
+    void invalidate()
+    {
+    }
+}
